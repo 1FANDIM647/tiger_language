0a7c9a2cb5254b39dba72e6ebf230bab5602ceea
Michael Elkin <55627341+1FANDIM647@users.noreply.github.com>
2020-01-15 19:53:56 +0300

    Add files via upload

--- tiger.c
+++ tiger.c
@@ -64,4 +64,85 @@ struct float {
     }
    }
   }
-  
+
+struct char {
+  // variable type data  char
+  char = var float ; // how we create variable char (type data)
+  size_max = 345690023525252452452400000230142,3424;// maximum size of variable
+  size_minimum = 0;// minimum size of variable
+  char  name ;
+  //function of input
+  void sinput(char   name) {
+    scanf(" %s\n",variable (x) );
+    float rand(x);
+    void write_in_memory();
+    void soutput(char  name){
+      //output of variable
+       printf("%s\n",variable (x));
+
+    }
+   }
+}
+
+struct long_int {
+
+  int = var int ; // how we create variable int (type data)
+  size_max = 345690000000014323243114313414;// maximum size of variable
+  size_minimum = 0;// minimum size of variable
+  int  name ;
+  //function of input
+  void sinput(int name) {
+    scanf(" %s\n",variable (x) );
+    int rand(x);
+    void write_in_memory();
+
+ void soutput(int name){
+   //output of variable
+    printf("%s\n",variable (x));
+
+
+}
+
+// faster descryption of  type data boolen
+struct bool{
+   bool = boolen ;
+}
+ // what about variable Kosh ,kosh , Cosh ,cosh ,кош
+void name_of_variable () {
+   string variable (x)
+   char  variable(x)
+   int variable (x)
+   long int variable(x)
+   double variable (x)
+   float variable (x)
+
+   switch ( variable ) {
+     case Kosh:
+     printf(" You Can't create this variable %s\n", );
+     break;
+     case kosh :
+      printf(" You Can't create this variable %s\n", );
+     break ;
+     case Cosh :
+       printf(" You Can't create this variable %s\n", );
+     break ;
+     case cosh :
+       printf(" You Can't create this variable %s\n", );
+     break ;
+   }
+
+}
+
+// system  function of input
+string  print ( string x , char x , int x , float x ,double x , long int x ,boolen x ; )
+{
+   scanf("%s\n",variable(x) );
+   printf("%s\n",variable(x));
+
+   return x ;
+}
+// system function of output
+string  output (string x , char x , int x , float x ,double x , long int x ,boolen x ;)
+{
+  return x;
+}
