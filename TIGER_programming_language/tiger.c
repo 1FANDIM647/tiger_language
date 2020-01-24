3f0529974def4561d54cbb4eb2225c03198bf5d3
Michael Elkin <55627341+1FANDIM647@users.noreply.github.com>
2020-01-24 23:10:59 +0300

    Add files via upload

---
+++ TIGER_programming_language/tiger.c
@@ -0,0 +1,158 @@
+/*
+LANGUAGE TIGER .
+Copyright (C) 2007 Free Software
+ Foundation GNU General Public License v3.0
+
+Developed by 2020
+
+*/
+#include <"stdlib.h">
+#include <"math.h">
+#include <"stdio.h">
+#include <"string.h">
+
+// descryption of "int" it is type data
+struct int {
+  int = var int ; // how we create variable int (type data)
+  size_max = 345690000000014;// maximum size of variable
+  size_minimum = 0;// minimum size of variable
+  int  name ;
+  //function of input
+  void sinput(int name) {
+    scanf(" %s\n",variable (x) );
+    int rand(x);
+    void write_in_memory();
+
+ void soutput(int name){
+   //output of variable
+    printf("%s\n",variable (x));
+
+
+ }
+}
+// descryption of "double" it is type data
+struct double {
+  double = var double ; // how we create variable double (type data)
+  size_max = 345690000000230142,3424;// maximum size of variable
+  size_minimum = 0;// minimum size of variable
+  double name ;
+  //function of input
+  void sinput(double name) {
+    scanf(" %s\n",variable (x) );
+    double rand(x);
+    void write_in_memory();
+  }
+ void soutput(double name){
+   //output of variable
+    printf("%s\n",variable (x));
+
+ }
+}
+// descryption of "float" it is type data
+struct float {
+  float = var float ; // how we create variable float (type data)
+  size_max = 345690023525252452452400000230142,3424;// maximum size of variable
+  size_minimum = 0;// minimum size of variable
+  float name ;
+  //function of input
+  void sinput(float  name) {
+    scanf(" %s\n",variable (x) );
+    float rand(x);
+    void write_in_memory();
+    void soutput(float name){
+      //output of variable
+       printf("%s\n",variable (x));
+
+    }
+   }
+  }
+
+struct char {
+  // variable type data  char
+  char = var float ; // how we create variable char (type data)
+  size_max = 345690023525252452452400000230142,3424;// maximum size of variable
+  size_minimum = 0;// minimum size of variable
+  char  name ;
+  //function of input
+  void sinput(char   name) {
+    scanf(" %s\n",variable (x) );
+    float rand(x);
+    void write_in_memory();
+    void soutput(char  name){
+      //output of variable
+       printf("%s\n",variable (x));
+
+    }
+   }
+}
+
+struct long_int {
+
+  int = var int ; // how we create variable int (type data)
+  size_max = 345690000000014323243114313414;// maximum size of variable
+  size_minimum = 0;// minimum size of variable
+  int  name ;
+  //function of input
+  void sinput(int name) {
+    scanf(" %s\n",variable (x) );
+    int rand(x);
+    void write_in_memory();
+
+ void soutput(int name){
+   //output of variable
+    printf("%s\n",variable (x));
+
+
+}
+
+// faster descryption of  type data boolen
+struct bool{
+   bool = boolen ;
+}
+ // what about variable Kosh ,kosh , Cosh ,cosh ,кош
+void name_of_variable () {
+   string variable (x)
+   char  variable(x)
+   int variable (x)
+   long int variable(x)
+   double variable (x)
+   float variable (x)
+
+   switch ( variable ) {
+     case Kosh:
+     printf(" You Can't create this variable %s\n", );
+     break;
+     case kosh :
+      printf(" You Can't create this variable %s\n", );
+     break ;
+     case Cosh :
+       printf(" You Can't create this variable %s\n", );
+     break ;
+     case cosh :
+       printf(" You Can't create this variable %s\n", );
+     break ;
+   }
+
+}
+
+// Available alphabet in the tiger
+struct Alphabet {
+
+    char signs_of_alphabet_132[] [] = {"A-Z" , "a-z ", "А-Я", "a-я" };
+    int  signs_of_alphabet_130[][] = {"0-9  " , "!@#$%^&*()_- "};
+    void name_of_variable();
+}
+
+// system  function of input
+string  print ( string x , char x , int x , float x ,double x , long int x ,boolen x ; )
+{
+   scanf("%s\n",variable(x) );
+   printf("%s\n",variable(x));
+
+   return x ;
+}
+// system function of output
+string  output (string x , char x , int x , float x ,double x , long int x ,boolen x ;)
+{
+  return x;
+}
